49771d74abe63786ac5e02600bdf3b8b059c131d
Long Shen <ls395@duke.edu>
2020-01-29 12:05:39 -0500

    Modify .py file

--- cholesterol_analysis.py
+++ cholesterol_analysis.py
@@ -1,12 +1,36 @@
+def HDL_analysis(HDL_level):
+    if HDL_level >= 60:
+        return "Normal"
+    elif 40 <= HDL_level < 60:
+        return "Borderline low"
+    else:
+        return "Low"
+
+
+
+
+def Cholesterol_analysis():
+    print("Cholesterol_analysis")
+    HDLinput = input("Enter test result: ")
+    test_info = HDLinput.split(" = ")
+    if test_info[0] == "HDL":
+        answer = HDL_analysis(int(test_info[1]))
+        print("The level is {}.".format(answer))
+
+
+
 def interface():
     choice = 0
     while choice != "9":
         print("Cholesterol Analysis")
         print("Options: ")
+        print("  1 - Cholesterol Analysis")
         print("  9 - Quit")
         choice = input("Enter your options: ")
         if choice == "9":
             return
+        elif choice == "1":
+            Cholesterol_analysis()
 
 if __name__ == "__main__":
     interface()
